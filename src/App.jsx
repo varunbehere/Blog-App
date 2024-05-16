1ab48f5baa482d61c2cb137877818b729f10b77f
Varun Behere <varunbehere@gmail.com>
2024-05-16 19:57:14 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Outlet } from 'react-router-dom'
-
-import Header from './Components/Header'
-import Footer from './Components/Footer'
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import { BlogContextProvider } from './Contexts/BlogContext';
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  const addBlog = (newBlog) => {
+    setBlogs(prev => [...prev, newBlog]);
+  };
+
+  const likeBlog = (id) => {
+    setBlogs(prev => prev.map(blog => blog.id === id ? { ...blog, likeCounter: (blog.likeCounter || 0) + 1 } : blog));
+  };
 
   return (
-    <div className=' w-full h-full'>
-      <Header/>
-      <div className='pt-24'><Outlet/></div>
-      <Footer/>
-    </div>
-  )
+    <BlogContextProvider value={{ blogs, addBlog, likeBlog }}>
+      <div className="w-full h-full">
+        <Header />
+        <div className="pt-24"><Outlet /></div>
+        <Footer />
+      </div>
+    </BlogContextProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Contexts/BlogContext.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+
+export const BlogContext = React.createContext({
+    blog : [
+        {   
+            id : any ,
+            content : "",
+            author : "",
+            likeCounter : 0,
+        }
+    ],
+    addBlog : (content,author)=>{},
+    likeBlog : (id)=>{}
+})
+
+export const BlogContextProvider = BlogContext.Provider()
+
+export const useBlogContext = ()=>{
+    return useContext(BlogContext)
+}
